f29289c1e938007ad86ec5413e7794d1e37a2a6d
Lio-RP <libanr4243@gmail.com>
2023-01-28 16:46:46 +0300

    Add some modifications to UserService.

--- src/main/java/com/electronic/shoppingrestapi/services/UserService.java
+++ src/main/java/com/electronic/shoppingrestapi/services/UserService.java
@@ -1,5 +1,6 @@
 package com.electronic.shoppingrestapi.services;
 
+import com.electronic.shoppingrestapi.auth.UserPrincipals;
 import com.electronic.shoppingrestapi.domain.User;
 
 public interface UserService {
@@ -7,4 +8,10 @@ public interface UserService {
     User findByUserName(String userName);
 
     User findByEmail(String email);
+
+    User register(User user);
+
+    User getCurrentlyLoggedUser(UserPrincipals userPrincipal);
+
+    boolean emailExists(String email);
 }
--- src/main/java/com/electronic/shoppingrestapi/services/UserServiceImpl.java
+++ src/main/java/com/electronic/shoppingrestapi/services/UserServiceImpl.java
@@ -1,16 +1,22 @@
 package com.electronic.shoppingrestapi.services;
 
+import com.electronic.shoppingrestapi.auth.UserPrincipals;
 import com.electronic.shoppingrestapi.domain.User;
 import com.electronic.shoppingrestapi.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -21,5 +27,34 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findByEmail(String email) {
         return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public User register(User user) {
+        User foundedUser = userRepository.findByUserName(user.getUserName());
+        if(emailExists(user.getEmail()) || foundedUser != null){
+            throw new RuntimeException("The email or username already exists");
+        }
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User getCurrentlyLoggedUser(UserPrincipals userPrincipal) {
+        if(userPrincipal == null) {
+            try {
+                throw new UserPrincipalNotFoundException("The user not founded");
+            } catch (UserPrincipalNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return userPrincipal.getUser();
+    }
+
+    @Override
+    public boolean emailExists(String email) {
+        return userRepository.findByEmail(email) != null;
     }
 }
